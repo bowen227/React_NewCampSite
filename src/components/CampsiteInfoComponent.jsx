2afccfd73e53008f0a6b9249494491da46654d86
Brent <brent.e.owen@gmail.com>
2021-04-10 13:22:55 -0400

    Added campsiteInfo component to render comments and details

---
+++ src/components/CampsiteInfoComponent.jsx
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderCampsite(campsite) {
+        return(
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </Card>
+            </div>
+        );
+    }
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => 
+                    <div key={comment.id} className="m-3">
+                        <span>{comment.text}</span>
+                        <br />
+                        <span>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</span>
+                    </div>)}
+                </div>
+            );
+        }
+        return <div></div>
+    }
+
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    }
+}
+
+export default CampsiteInfo;
